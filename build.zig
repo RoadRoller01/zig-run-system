e5ec2e9c5b4ac16cdb51d03fa98a5bd64e24e7a3
RoadRoller01 <roadroller01mail@proton.me>
2025-06-11 16:18:12 +0300

    Mount self, foreign and stdlib source directories via 9p protocol

--- build.zig
+++ build.zig
@@ -1,21 +1,27 @@
 const std = @import("std");
-const Cpio = @import("cpio.zig");
+const Cpio = @import("./cpio.zig");
 const Step = std.Build.Step;
 
-pub fn addRunSystem(hisB: *std.Build) *std.Build.Step.Run {
-    const build_root_path = hisB.build_root.path orelse std.fs.cwd().realpathAlloc(hisB.allocator, ".") catch unreachable;
-    const dir_name = std.fs.path.basename(build_root_path);
+pub fn addRunSystem(hisB: *std.Build, argv: []const []const u8) *std.Build.Step.Run {
+    const hisBDir = hisB.build_root.path orelse std.fs.cwd().realpathAlloc(hisB.allocator, ".") catch unreachable;
+    const dir_name = std.fs.path.basename(hisBDir);
 
     const b =
-        if (std.mem.eql(u8, dir_name, "zig-run-system"))
+        if (std.mem.startsWith(u8, dir_name, "zig-run-system"))
             hisB
         else
             hisB.dependency("zig_run_system", .{}).builder;
 
+    const bDir =
+        if (b == hisB)
+            null
+        else
+            b.build_root.path orelse std.fs.cwd().realpathAlloc(b.allocator, ".") catch unreachable;
+
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const init_mod = b.createModule(.{
+    const init_mod = hisB.createModule(.{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
@@ -24,13 +30,18 @@ pub fn addRunSystem(hisB: *std.Build) *std.Build.Step.Run {
         .name = "init",
         .root_module = init_mod,
     });
+
     // b.installArtifact(init);
+
+    const builders = .{ hisB, b };
+
+    std.debug.print("{any}", .{@TypeOf(builders)});
 
     const initramfs_step = b.allocator.create(Step) catch @panic("OOM");
     initramfs_step.* = Step.init(.{
         .name = "GenerateInitramfs",
         .id = .custom,
-        .owner = b,
+        .owner = builders[1],
         .makeFn = make,
     });
     initramfs_step.dependOn(&init.step);
@@ -44,18 +55,53 @@ pub fn addRunSystem(hisB: *std.Build) *std.Build.Step.Run {
     });
 
     call_qemu.addArgs(if (target.result.os.tag == .linux) switch (target.result.cpu.arch) {
-        .aarch64 => &.{ "-kernel", b.path("images/Image.gz").src_path.sub_path, "-M", "virt", "-cpu", "cortex-a53", "-append", "console=ttyAMA0 init=\\init" },
-        .x86_64 => &.{ "-kernel", b.path("images/bzImage").src_path.sub_path, "-append", "console=ttyS0 init=\\init" },
+        .aarch64 => &.{
+            "-kernel",
+            b.path("images/Image.gz").src_path.sub_path,
+            "-M",
+            "virt",
+            "-cpu",
+            "cortex-a53",
+            "-append",
+            b.fmt("console=ttyAMA0 init=\\init {s} {s} {s} {s} ", .{ hisBDir, b.graph.zig_lib_directory.path.?, bDir orelse "none", std.mem.join(b.allocator, " ", argv) catch unreachable }),
+        },
+        .x86_64 => &.{
+            "-kernel",
+            b.path("images/bzImage").src_path.sub_path,
+            "-append",
+            b.fmt("console=ttyS0 init=\\init {s} {s} {s} {s}", .{ hisBDir, b.graph.zig_lib_directory.path.?, bDir orelse "none", std.mem.join(b.allocator, " ", argv) catch unreachable }),
+        },
         else => @panic("linux arch is not added"),
     } else @panic("os is not added"));
 
-    call_qemu.addArg("-display");
-    call_qemu.addArg("none");
-    call_qemu.addArg("-serial");
-    call_qemu.addArg("mon:stdio");
-    call_qemu.addArg("-initrd");
+    call_qemu.addArgs(&.{
+        "-display",
+        "none",
+        "-serial",
+        "mon:stdio",
+        "-initrd",
+    });
     call_qemu.addFileArg(initramfs_file);
 
+    // mount parnet project dir
+    call_qemu.addArgs(&.{
+        "-virtfs",
+        b.fmt("local,path={s},mount_tag=host0,security_model=mapped,id=host0", .{hisBDir}),
+    });
+
+    // mount zig lib dir
+    call_qemu.addArgs(&.{
+        "-virtfs",
+        b.fmt("local,path={s},mount_tag=host1,security_model=mapped,id=host1", .{b.graph.zig_lib_directory.path.?}),
+    });
+
+    if (bDir) |dir| {
+        // mount our project dir
+        call_qemu.addArgs(&.{
+            "-virtfs",
+            b.fmt("local,path={s},mount_tag=host2,security_model=mapped,id=host2", .{dir}),
+        });
+    }
     call_qemu.step.dependOn(initramfs_step);
 
     return call_qemu;
@@ -81,11 +127,14 @@ fn make(step: *Step, options: Step.MakeOptions) anyerror!void {
 
     var cpio = try Cpio.init(&initramfs_stream);
     try cpio.addFile("init", &init_stream, 0o755);
+
     try cpio.finalize();
 }
 
 pub fn build(b: *std.Build) void {
-    var test_step = b.step("test", "Run tests");
-    test_step.dependOn(&addRunSystem(b).step);
+    if (b.pkg_hash.len == 0) {
+        var test_step = b.step("test", "Run tests");
+        test_step.dependOn(&addRunSystem(b, &.{ "tajreba", "jamela" }).step);
+    }
     return;
 }
--- src/main.zig
+++ src/main.zig
@@ -16,6 +16,12 @@ pub fn main() !void {
         return;
     }
 
+    var args = try std.process.argsAlloc(std.heap.page_allocator);
+    const host0Dir = args[1];
+    const host1Dir = args[2];
+    const host2Dir = args[3];
+    const argv = args[4..];
+
     log.info("uname: {s} {s} {s} {s} {s} {s}", std.posix.uname());
 
     const root = try fs.openDirAbsolute("/", .{});
@@ -25,6 +31,30 @@ pub fn main() !void {
     try root.makeDir("sys");
     try mount("none", "/proc", "proc", 0, 0);
     try mount("none", "/sys", "sysfs", 0, 0);
+
+    const data: [*:0]const u8 = "trans=virtio,version=9p2000.L";
+    log.info("Making Path {s}", .{host0Dir});
+    try root.makePath(host0Dir[1..]);
+    try mount("host0", host0Dir, "9p", 0, @intFromPtr(data));
+
+    log.info("Making Path {s}", .{host1Dir});
+    try root.makePath(host1Dir[1..]);
+    try mount("host1", host1Dir, "9p", 0, @intFromPtr(data));
+
+    if (host2Dir[0] != 'n') {
+        log.info("Making Path {s}", .{host2Dir});
+        try root.makePath(host2Dir[1..]);
+        try mount("host2", host2Dir, "9p", 0, @intFromPtr(data));
+    }
+
+    log.info("Calling: {s}", .{try std.mem.join(std.heap.page_allocator, " ", argv)});
+    const child = try std.process.Child.run(.{
+        .allocator = std.heap.page_allocator,
+        .argv = argv,
+        .cwd = host0Dir,
+    });
+
+    log.info("Calling reault: {any}", .{child.term});
 }
 
 fn mount(
@@ -40,7 +70,27 @@ fn mount(
     if (ret == std.posix.E.SUCCESS)
         return;
 
-    log.err("mount({s}, {s}, {s}, {}, {}) = {}", .{ special, dir, fstype, flags, data, ret });
+    log.err("mount({s}, {s}, {s}, {}, {x}) = {}", .{ special, dir, fstype, flags, data, ret });
 
     return error.mountFailed;
+}
+
+fn ls(path: []const u8) !void {
+    var dir = try fs.openDirAbsolute(path, .{});
+    var idir = try dir.openDir(path, .{ .iterate = true });
+    defer idir.close();
+    var itr = idir.iterate();
+
+    log.info("ls {s}", .{path});
+
+    while (try itr.next()) |entry| {
+        const t = switch (entry.kind) {
+            .directory => "dir",
+            .file => "file",
+            .block_device, .character_device => "dev",
+            else => "other",
+        };
+
+        log.info("{s:5}: {s}", .{ t, entry.name });
+    }
 }
